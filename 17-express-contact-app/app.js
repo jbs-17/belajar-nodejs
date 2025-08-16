2ba15c67bc573d15bba14741d4e538db36c90e78
jatibanguns <jatibangun17@gmail.com>
2025-08-16 22:47:28 +0700

    menerapkan ilmu nodejs dasar untuk membuat contact app

---
+++ 17-express-contact-app/app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const expressLayout = require('express-ejs-layouts');
+
+
+
+app.set('view engine', 'ejs');
+app.use(expressLayout);
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+  let mahasiswa = [
+    {
+      name: 'Jati B. S.',
+      email: 'jbs@email.com'
+    },
+    {
+      name: 'Radtya Ahnaf M',
+      email: 'radit.ar@email.com'
+    },
+    {
+      name: 'Novanto Rokim',
+      email: 'nova.ro@email.com'
+    },
+  ]
+  res.render('index', { layout: 'layouts/main-layout.ejs', nama: 'Express Web Server', title: 'Home Page', mahasiswa });
+});
+app.get('/contact', (req, res) => {
+  res.render('contact', { layout: 'layouts/main-layout.ejs', title: 'Contect Page' });
+});
+app.get('/about', (req, res) => {
+  res.render('about', { layout: 'layouts/main-layout.ejs', title: 'About Page' });
+});
+app.get('/search' , (req, res)=>{
+  const { q } = req.query;
+  if(!q)return res.send('query tidak valid');
+  res.send(`anda mencari ${q}`);
+})
+
+
+app.use((req, res, next) => {
+  res.status(404).send("Maaf, halaman tidak ditemukan.");
+});
+
+
+
+app.listen(port, () => {
+  console.log(`App listening at port ${port}`);
+});
